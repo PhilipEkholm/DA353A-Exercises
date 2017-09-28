b1896dcdfeb92d75b88bf874ef85084fc5339c4a
Philip Ekholm <philipekholm@protonmail.com>
2017-09-28 21:13:05 +0200

    Initial commit for project import

---
+++ src/laboration3/SortObjects.java
@@ -0,0 +1,56 @@
+package laboration3;
+
+import arrays.ArraySupporter;
+
+public class SortObjects {
+	public static void main(String[] args) {
+		int[] array = ArraySupporter.generateRandomArray(5, -5, 5);
+		
+		System.out.println(ArraySupporter.toString(array));
+		SortObjects.bubbleSortObjects(array);
+		System.out.println(ArraySupporter.toString(array));
+	}
+	
+	public static void bubbleSortObjects(int[] array){
+		for(int i = 0; i < array.length; i++){
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
